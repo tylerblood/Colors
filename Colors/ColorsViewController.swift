669e788562ea8b0c840162c3fa3cf6874e072a20
Tyler Blood <tylerblood@mwc-122096.dhcp.missouri.edu>
2019-03-08 14:42:46 -0600

    Working code

--- Colors/ColorsViewController.swift
+++ Colors/ColorsViewController.swift
@@ -8,12 +8,30 @@
 
 import UIKit
 
-class ColorsViewController: UIViewController {
+class ColorsViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    var colors = ["red", "orange", "yellow", "green", "blue", "purple", "brown"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return colors.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "colorCell", for: indexPath)
+        
+        cell.textLabel?.text = colors[indexPath.row]
+        
+        return cell
     }
     
 
